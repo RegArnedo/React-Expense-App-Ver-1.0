c07a4cb12f19b8624b1baa6f03f2b364199904f8
RegArnedo <99155254+RegArnedo@users.noreply.github.com>
2022-09-07 17:52:30 -0700

    Updating main after multi components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-// Make our components resuable via parameters and prop usage
-//
 function App() {
   const expenses = [
     {
@@ -24,29 +22,11 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }
